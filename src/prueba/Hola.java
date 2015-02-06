4f36c489ca6f5ef33ea86d7f2b126cebbef55f7b
UlyssesOrtega <UlyssesOrtega@192.168.1.11>
2015-02-06 17:25:10 +0000

    17:25

--- src/prueba/Hola.java
+++ src/prueba/Hola.java
@@ -1,18 +1,12 @@
 package prueba;
 
 public class Hola {
-/*
- *
- * nuevo cambio 11.56
- * segundo commit a las 12.00
- * 
- */
 	
 	private int dedo;
 	
+	//constructor
 	public Hola() {
 		this.dedo = 5;
 	}
-	
 	
 }
